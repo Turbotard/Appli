70f8b04a385fe26f3b96a49b69674554670f2192
Turbotard <turbotard9@gmail.com>
2022-04-07 17:52:41 +0200

    Co-authored-by: rermler <rermler@users.noreply.github.com>

--- py.py
+++ py.py
@@ -1,3 +1,10 @@
-print("oui")
-print("amogus")
-print("test de ryles")
+def vente_marche_noir():
+    age = int(input("quel age ? "))
+    nombre = int(input("combien d'enfants voulez vous ? "))
+    assert age < 90, "Trop vieux"
+    assert nombre < 10, "Trop nombreux"
+    if age < 18:
+        return f"Cela vous fera donc {nombre*50*1.2}€"
+    else:
+        return f"Cela vous fera donc {nombre*75*1.2}€"
+print(vente_marche_noir())
